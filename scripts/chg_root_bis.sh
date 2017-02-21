07a6edabe27ccf3ee333e13567681415d35f404d
root <root@Millenium.starlabs.local>
2017-02-21 18:49:22 +0100

    system configuration completed

---
+++ scripts/chg_root_bis.sh
@@ -0,0 +1,11 @@
+mount -v --bind /dev $LFS/dev
+mount -vt devpts devpts $LFS/dev/pts -o gid=5,mode=620
+mount -vt proc proc $LFS/proc
+mount -vt sysfs sysfs $LFS/sys
+mount -vt tmpfs tmpfs $LFS/run
+chroot "$LFS" /tools/bin/env -i \
+    HOME=/root                  \
+    TERM="$TERM"                \
+    PS1='\u:\w\$ '              \
+    PATH=/bin:/usr/bin:/sbin:/usr/sbin \
+    /bin/bash --login
